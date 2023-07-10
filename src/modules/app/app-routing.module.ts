a10643215363d6969c0d6db3787bc7b974f5391d
kenjr <kenedy@myhechar.pro>
2023-07-10 18:00:44 +0300

    Approuting - intro'd 2 new routes for the landing and auth's children

--- src/modules/app/app-routing.module.ts
+++ src/modules/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './presentation/not-found/not-found.component';
 import { landingPageRoutes } from '../landing/landing-routing.module';
+import { authRoutes } from '../auth/auth-routing.module';
 
 const routes: Routes = [
-  {path: "", children:landingPageRoutes},
+  {path: "", children: landingPageRoutes},
+  {path: "auth", children: authRoutes},
   {path: "**", component: NotFoundComponent}
 ];
 
