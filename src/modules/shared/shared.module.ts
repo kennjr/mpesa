5516ea84b1b4e9205bc618021786a5f31b369928
kenjr <kenedy@myhechar.pro>
2023-07-10 17:58:28 +0300

    SharedModule - exported the LogoSmall component

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoSmallComponent } from './components/logo-small/logo-small.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LogoSmallComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LogoSmallComponent
   ]
 })
 export class SharedModule { }
