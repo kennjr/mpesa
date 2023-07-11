9a521a4d804b153178b2669109ebb489c6037a91
kenjr <kenedy@myhechar.pro>
2023-07-11 10:48:44 +0300

    SharedModule - exported the created components

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoSmallComponent } from './components/logo-small/logo-small.component';
+import { LogoHeaderComponent } from './components/logo-header/logo-header.component';
 
 
 
 @NgModule({
   declarations: [
-    LogoSmallComponent
+    LogoSmallComponent,
+    LogoHeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    LogoSmallComponent
+    LogoSmallComponent,
+    LogoHeaderComponent
   ]
 })
 export class SharedModule { }
