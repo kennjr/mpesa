3b33d4a3d7405d9d54b61b5a1ef4d74188e35f0b
kenjr <kenedy@myhechar.pro>
2023-07-13 00:20:22 +0300

    AppRoutingModulets - intro'd the app paths to the routes list

--- src/modules/app/app-routing.module.ts
+++ src/modules/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './presentation/not-found/not-found.component';
+import { NotFoundComponent } from './presentation/pages/not-found/not-found.component';
 import { landingPageRoutes } from '../landing/landing-routing.module';
 import { authRoutes } from '../auth/auth-routing.module';
+import { DashboardComponent } from './presentation/pages/dashboard/dashboard.component';
+import { HistoryComponent } from './presentation/pages/history/history.component';
+import { ProfileComponent } from './presentation/components/profile/profile.component';
+import { NewTransactionComponent } from './presentation/components/new-transaction/new-transaction.component';
+import { TransactionInfoComponent } from './presentation/components/transaction-info/transaction-info.component';
+import { FundAccountComponent } from './presentation/components/fund-account/fund-account.component';
 
 const routes: Routes = [
-  {path: "", children: landingPageRoutes},
+  {path: "app", component: DashboardComponent, 
+    children: [
+      {path: "history", component: HistoryComponent},
+      {path: "profile", component: ProfileComponent},
+      {path: "load", component: FundAccountComponent},
+      {path: "transaction", component: TransactionInfoComponent},
+      {path: "transact", component: NewTransactionComponent},
+    ]},
   {path: "auth", children: authRoutes},
+  {path: "", children: landingPageRoutes},
   {path: "**", component: NotFoundComponent}
 ];
 
