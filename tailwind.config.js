a8e095788820a58c09783bf96771e76b229dc678
kenjr <kenedy@myhechar.pro>
2023-07-13 00:23:51 +0300

    TailwindConfig - intro'd the aspect-ration plugin and some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,14 @@ module.exports = {
       colors: ({ colors }) => ({
         gray: colors.neutral,
         "primary": "#C8ABFF",
-        "app-pink": "#F3ACFF"
+        "app-pink": "#F3ACFF",
+        "app-pink-light": "#ffeef9"
       })
     },
   },
   plugins: [
     require('@tailwindcss/typography'),
-    // require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
   ],
 }
