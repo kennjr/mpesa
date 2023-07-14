99802b113c3f7493aaad826fcae415a824cf9df3
kenjr <kenedy@myhechar.pro>
2023-07-14 06:53:53 +0300

    TransactionsDto.ts - created more interfaces in the file

--- src/modules/app/data/dtos/transactionsDto.ts
+++ src/modules/app/data/dtos/transactionsDto.ts
@@ -1,4 +1,41 @@
+import { Timestamp } from "@angular/fire/firestore";
+
 export interface TransactionsDto{
     requestFrom: number;
     pageNumber: number;
+}
+
+type BaseTransaction = {
+    tid: string;
+    sender: MyUser;
+    receiver: MyUser;
+    timestamp: Timestamp;
+    amount: number;
+    fee: number;
+    result: boolean;
+}
+
+export interface MyTransaction {
+    senderBalance: number;
+    receiverBalance: number;
+    receiverEmail: string;
+    senderEmail: string;
+    receiverName: string;
+    senderName: string;
+    receiverPhone: string;
+    senderPhone: string;
+    timestamp: Timestamp;
+    amount: number;
+    fee: number;
+    result: boolean;
+}
+
+export interface MyUser {
+    name: string | null;
+    uid: string;
+    location: string | null;
+    email: string | null;
+    phone: string | null;
+    timestamp: Timestamp;
+    balance: number;
 }
