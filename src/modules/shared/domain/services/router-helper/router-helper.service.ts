2ed9bc8f3f414a656d312461bb59c971ae362901
kenjr <kenedy@myhechar.pro>
2023-07-14 07:31:46 +0300

    Removed unneeded code

--- src/modules/shared/domain/services/router-helper/router-helper.service.ts
+++ src/modules/shared/domain/services/router-helper/router-helper.service.ts
@@ -1,7 +1,5 @@
-import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +7,8 @@ import { Subscription } from 'rxjs';
 export class RouterHelperService {
 
   private validRouteRegices
-  private locationSub!: Subscription
 
-  constructor(private router: Router, private location: Location) {
+  constructor(private router: Router) {
 
     const validRoutes:string[] = []
 
