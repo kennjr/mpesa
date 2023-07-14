46350e2817a17dc22b786cc6c4d154308443cfb0
kenjr <kenedy@myhechar.pro>
2023-07-14 07:20:19 +0300

    HeaderComponent - added clicks and emmitters that'll notify the landingpage of any scrolls, through a service

--- src/modules/landing/presentation/components/header/header.component.ts
+++ src/modules/landing/presentation/components/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HeaderEmmissions } from 'src/modules/landing/common/LandingUtils';
+import { ScrollData } from 'src/modules/shared/data/dtos/ScrollData';
+import { SharedRepo } from 'src/modules/shared/data/repos/SharedRepo';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +12,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  route:string = "";
+  locationSub!: Subscription;
+
+  shardRepo = inject(SharedRepo);
+  currentRoute = inject(ActivatedRoute);
+
+  @Output() scrollToSection = new EventEmitter<number>();
+
+  constructor(private router: Router, private location: Location){
+    this.locationSub = router.events.subscribe(val => {
+      if (location.path() != "") {
+        this.route = location.path();
+      } else {
+        this.route = "";
+      }
+    });
+  }
+
+  public onFeaturesClicked (): void{
+    this.navigationRequest(HeaderEmmissions.Features);
+  }
+
+  public onTestimonialsClicked (): void{
+    this.navigationRequest(HeaderEmmissions.Testimonials);
+  }
+
+  public onTeamClicked (): void{
+    this.navigationRequest(HeaderEmmissions.Team);
+  }
+  
+  public onPricingClicked (): void{
+    this.navigationRequest(HeaderEmmissions.Pricing);
+  }
+
+  private navigationRequest(sectionId: number): void {
+    if(this.route.trim().length > 0){
+      // simply navigate back home
+      this.goHome();
+    }else{
+      // the user is at home, emit the value so that he can be scrolled to the appropriate section
+      this.scrollToSection.emit(sectionId);
+      let newScrollingState: ScrollData = { isScrolling: true, scrollTarget: sectionId };
+      this.shardRepo.updateScrollingState(newScrollingState);
+    }
+  }
+
+  private goHome (): void{
+    this.router.navigate(['/'], {relativeTo: this.currentRoute});
+  }
+
 
 }
