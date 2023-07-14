ed253e23aa4fdff1b077c403b99f9405414e8a64
kenjr <kenedy@myhechar.pro>
2023-07-14 07:04:39 +0300

    TransactionsComponent - wrote code that fetches and displays the data from the repo

--- src/modules/app/presentation/components/transactions/transactions.component.ts
+++ src/modules/app/presentation/components/transactions/transactions.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, inject } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { DocumentData } from '@angular/fire/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TransactionsDto } from 'src/modules/app/data/dtos/transactionsDto';
+import { SharedRepo } from 'src/modules/shared/data/repos/SharedRepo';
 
 @Component({
   selector: 'app-transactions',
@@ -10,15 +13,37 @@ import { TransactionsDto } from 'src/modules/app/data/dtos/transactionsDto';
 export class TransactionsComponent implements OnInit, OnDestroy {
 
   @Input() public componentData!: TransactionsDto;
+  @Input() transactions: DocumentData[] = [];
+  @Input() userEmail: string = "";
   @Output() goToHistory = new EventEmitter<number>();
 
+  @Output() filterTransactions = new EventEmitter<string>();
+
   public anyDataAvailable: boolean = true;
+  public transactionsFilter: string = "all";
+
+  sharedRepo = inject(SharedRepo);
 
   public isLoading: boolean = true;
 
-  constructor(private router: Router, private route: ActivatedRoute){}
+  constructor(){}
 
   ngOnInit(): void {
+    this.sharedRepo.getTransactionsList().subscribe({
+        next: (val: DocumentData[]) => {
+          if(val.length > 0){
+            this.anyDataAvailable = true;
+          }else{
+            this.anyDataAvailable = false;
+          }
+          this.transactions = val;
+          console.log("The data", val);
+        },
+        error: (error) => {
+          // TODO : show a msg when we get this error
+          console.log("We got an error", error);
+        }
+      })
     if(!this.componentData){
       // TODO : don't make a request
       this.isLoading = false;
@@ -30,14 +55,22 @@ export class TransactionsComponent implements OnInit, OnDestroy {
       }, 3000);
     }
   }
+
   ngOnDestroy(): void {
      // TODO : unsubscribe from subs
   }
 
-  public openTransactionInfo(id: number): void{
+  public onFilterTransactionsClicked (type: string): void{
+    this.filterTransactionRequest(type);
+  } 
 
+  private filterTransactionRequest (type: string): void{
+    if(type == "all" || type == "sent" || type == "received"){
+      this.filterTransactions.emit(type);
+      this.transactionsFilter = type;
+    }
   }
-   
+
   public showTransactionHistory(): void{
     this.onGoToHistory();
   }
