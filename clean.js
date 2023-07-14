268363d40bf04cb466ba1c20200e58c47479f9e0
kenjr <kenedy@myhechar.pro>
2023-07-14 18:37:40 +0300

    Minor changes meant to fix the auth bug in the delpoyed site

---
+++ clean.js
@@ -0,0 +1,14 @@
+// import { Tree } from '@nx/devkit';
+
+// function clean(tree) {
+//   // Perform cleanup tasks here
+//   // For example, you can delete specific directories/files using Tree APIs
+//   tree.delete('dist');
+//   tree.delete('node_modules');
+// }
+
+// export const description = 'Custom clean generator';
+// export async function run(host, options) {
+//     const tree = new Tree(host);
+//     clean(tree);
+// }
