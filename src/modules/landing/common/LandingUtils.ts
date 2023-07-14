4205c5abc1284cdce9c82d4e0c38209b51258501
kenjr <kenedy@myhechar.pro>
2023-07-14 07:19:01 +0300

    LandingUtils - added an interface that keeps track of all the scrollable sections of the landing page

---
+++ src/modules/landing/common/LandingUtils.ts
@@ -0,0 +1,6 @@
+export class HeaderEmmissions {
+    public static  Features: number = 1;
+    public static  Pricing: number = 2;
+    public static  Team: number = 3;
+    public static  Testimonials: number = 4;
+}
