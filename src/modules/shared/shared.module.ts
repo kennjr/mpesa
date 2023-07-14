0f547e7bc7f7f3a8f23e7c389820e79d0ab7fba6
kenjr <kenedy@myhechar.pro>
2023-07-14 07:30:22 +0300

    SharedModule - exported the newly created components and directives

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoSmallComponent } from './components/logo-small/logo-small.component';
 import { LogoHeaderComponent } from './components/logo-header/logo-header.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { OnlyNumbersDirective } from './domain/directives/only-numbers/only-numbers.directive';
 
 
 
 @NgModule({
   declarations: [
     LogoSmallComponent,
-    LogoHeaderComponent
+    LogoHeaderComponent,
+    ModalComponent,
+    OnlyNumbersDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
     LogoSmallComponent,
-    LogoHeaderComponent
+    LogoHeaderComponent,
+    ModalComponent,
+    OnlyNumbersDirective
   ]
 })
 export class SharedModule { }
